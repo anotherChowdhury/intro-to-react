8fd490ad1d63bfecfb1159437260a49dbbc4d25f
Umar Faruk <umarfauk.chowdhury@gmail.com>
2020-05-17 00:26:31 +0600

    React Intro Fronedn Masters Brian Holt

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const Pet = ({
+    name,
+    animal,
+    breed
+}) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed),
+    ]);
+};
+const App = () => {
+    return React.createElement(
+        "div", {
+            id: "passing-attributes",
+        },
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "Dog",
+                breed: "Havanese",
+            }),
+            React.createElement(Pet, {
+                name: "Peper",
+                animal: "Bird",
+                breed: "Cockatiel",
+            }),
+            React.createElement(Pet, {
+                name: "Doink",
+                animal: "Cat",
+                breed: "Mixed",
+            }),
+        ]
+    );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
