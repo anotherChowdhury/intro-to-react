2620fc7f9764d50c5a701474b801fdbcb14feaed
Umar Faruk <umarfauk.chowdhury@gmail.com>
2020-05-17 15:06:18 +0600

    First Commit,Eslint-ReactHooks not working

--- src/app.js
+++ src/app.js
@@ -1,38 +1,39 @@
-const Pet = ({
-    name,
-    animal,
-    breed
-}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 const App = () => {
-    return React.createElement(
-        "div", {
-            id: "passing-attributes",
-        },
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog",
-                breed: "Havanese",
-            }),
-            React.createElement(Pet, {
-                name: "Peper",
-                animal: "Bird",
-                breed: "Cockatiel",
-            }),
-            React.createElement(Pet, {
-                name: "Doink",
-                animal: "Cat",
-                breed: "Mixed",
-            }),
-        ]
-    );
+  // return React.createElement(
+  //     "div", {
+  //         id: "passing-attributes",
+  //     },
+  //     [
+  //         React.createElement("h1", {}, "Adopt Me!"),
+  //         React.createElement(Pet, {
+  //             name: "Tommy",
+  //             animal: "Dog",
+  //             breed: "Havanese",
+  //         }),
+  //         React.createElement(Pet, {
+  //             name: "Peper",
+  //             animal: "Bird",
+  //             breed: "Cockatiel",
+  //         }),
+  //         React.createElement(Pet, {
+  //             name: "Doink",
+  //             animal: "Cat",
+  //             breed: "Mixed",
+  //         }),
+  //     ]);
+  return (
+    <div>
+      <h1 id="passing props">Adopt Me</h1>
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
